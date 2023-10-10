725af7db7962bed4687b0c668241e677a703cbfa
Nikita Sergeev <sergeevn660@gmail.com>
2023-10-11 01:07:37 +0300

    Убраны ненужные методы, поля сделаны константными

--- Java_LR3/src/Worker.java
+++ Java_LR3/src/Worker.java
@@ -1,35 +1,11 @@
 public class Worker {
-    private String surname;
-    private String position;
-    private String gender;
-    private int birthYear;
+    private final String surname;
+    private final String position;
+    private final String gender;
+    private final int birthYear;
 
-    public String getSurname() {
-        return surname;
-    }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getBirthYear() {
-        return birthYear;
-    }
-    public void setBirthYear(int birthYear) {
-        this.birthYear = birthYear;
+    public String getAllData() {
+        return surname + " " + position + " " + gender + " " + birthYear + "\n";
     }
 
     public Worker(String surname, String position, String gender, int birthYear) {
