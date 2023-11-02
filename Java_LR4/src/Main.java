b5af48e7c0b8c708dca40d3e7ce7c19d7f417e40
Nikita Sergeev <sergeevn660@gmail.com>
2023-11-02 18:00:38 +0300

    Создано меню и основа для ключей

--- Java_LR4/src/Main.java
+++ Java_LR4/src/Main.java
@@ -1,29 +1,75 @@
-import javax.sql.rowset.spi.SyncResolver;
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        while (true) {
-//            System.out.println("Lab.Work #4");
-//            if (args[0].equals("-h") || args[0].equals("--help")) {
-//                System.out.println("Использование: java Main <имя_текстового_файла>");
-//            } else {
-//                System.out.println("Справка: java Main -h или java Main --help");
-//            }
+
+        System.out.println("\n\t\t\t=== Лабораторная работа #4 ===\n\t\tВыполнил студент группы ИКПИ-14 Сергеев Н.В.\n");
+        while (true) {
+
+            if (args.length == 0) {
+                showIncorrectInputMessage();
+                break;
+            }
+
+            else {
+                switch (args[0]) {
+                    case "--csv":
+                        if (args[1].isEmpty()) {
+                            System.out.println("Отсутствует имя исходного файла.");
+                            System.out.println("Введите его после ключа '--csv' и повторите попытку.");
+                            return;
+                        } else {
+                            writeWordFrequencyToCSV(Objects.requireNonNull(createWordFrequencyMap(args[1])), args[1]);
+                            // Objects.requireNonNull Позволяет гарантированно получить Not null объект на выходе
+                        }
+                        return;
+
+                    case "--csv-few":
+                        return;
+
+                    case "--json-write":
+                        return;
+
+                    case "--json-read":
+                        return;
+
+                    case "--help":
+                        showMenu();
+                        return;
+
+                    default:
+                        showIncorrectInputMessage();
+                        return;
+                }
+            }
+        }
+
+//        for (String fileName : args) {
+//            Map<String, Integer> wordFrequencyMap = createWordFrequencyMap(fileName);
 //        }
-        for (String fileName : args) {
-            Map<String, Integer> wordFrequencyMap = createWordFrequencyMap(fileName);
-        }
-        String inputFileName = args[0];
-        Map<String, Integer> wordFrequencyMap = createWordFrequencyMap(inputFileName);
-        if (wordFrequencyMap != null) {
-            writeWordFrequencyToCSV(wordFrequencyMap);
-        }
+//        String inputFileName = args[0];
+//        Map<String, Integer> wordFrequencyMap = createWordFrequencyMap(inputFileName);
+//        if (wordFrequencyMap != null) {
+//            writeWordFrequencyToCSV(wordFrequencyMap);
+//        }
     }
 
-    public static Map<String, Integer> createWordFrequencyMap(String inputFileName) {
-        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+    public static void showMenu() {
+        System.out.println("\t\t\t=== Возможности программы ===\n");
+        System.out.println("\tЗапись в CSV-файл\t\t\t\t| --csv <имя файла>");
+        System.out.println("\tЗапись в CSV-файл для n-ого количества файлов\t| --csv-few <имя файла> <...>");
+        System.out.println("\tЗапись JSON-файла для n-ого количества файлов\t| --json-write <имя файла> <...>");
+        System.out.println("\tЧтение JSON-файла\t\t\t\t| --json-read <имя файла>");
+        System.out.println("\tСправка\t\t\t\t\t\t| --help\n");
+    }
+
+    public static void showIncorrectInputMessage() {
+        System.out.println("\tЧтобы посмотреть справку по работе с программой, введите: java Main --help\n");
+    }
+
+    public static HashMap<String, Integer> createWordFrequencyMap(String inputFileName) {
+        HashMap<String, Integer> wordFrequencyMap = new HashMap<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))){
             String line;
@@ -45,13 +91,12 @@ public class Main {
             }
         } catch (IOException e) {
             System.err.println("Ошибка при чтении файла: " + e.getMessage());
-            return null;
         }
         return wordFrequencyMap;
     }
 
-    public static void writeWordFrequencyToCSV(Map<String, Integer> wordFrequencyMap) {
-        List<Map.Entry<String, Integer>> sortedWordFrequencyList = new ArrayList<>(wordFrequencyMap.entrySet());
+    public static void writeWordFrequencyToCSV(HashMap<String, Integer> wordFrequencyMap, String inputFileName) {
+        ArrayList<Map.Entry<String, Integer>> sortedWordFrequencyList = new ArrayList<>(wordFrequencyMap.entrySet());
 //        sortedWordFrequencyList.sort(Comparator.comparingInt(entry -> -entry.getValue()));
         sortedWordFrequencyList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
 
@@ -59,29 +104,29 @@ public class Main {
         int mostCommonWordCount = sortedWordFrequencyList.get(0).getValue();
         String rarestWord = sortedWordFrequencyList.get(sortedWordFrequencyList.size() - 1).getKey();
         int rarestWordCount = sortedWordFrequencyList.get(sortedWordFrequencyList.size() - 1).getValue();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("word_frequency.csv"))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(inputFileName + "_frequency_analysis.csv"))){
 
             writer.write("Word,Frequency,Frequency in %,Most common word,Count of most common word,Rarest word,Count of rarest word");
             writer.newLine();
 
-            for (Map.Entry<String, Integer> entry : sortedWordFrequencyList) {
+            for (HashMap.Entry<String, Integer> entry : sortedWordFrequencyList) {
                 String word = entry.getKey();
                 int count = entry.getValue();
                 // Округляем число до целого, затем умножаем на 100 и делим на 100, чтобы получить число с точностью до 2 знаков после запятой
                 double frequencyPercent = Math.round((double) count * 100 / wordFrequencyMap.size() * 100.0) / 100.0;
                 String percentWithPoint = String.valueOf(frequencyPercent).replace(',', '.') + "%";
                 if (word.equals(mostCommonWord)) {
-                    writer.write(String.format("%s, %d, %s, %s, %d, %s, %d", word, count, percentWithPoint, mostCommonWord, mostCommonWordCount, rarestWord, rarestWordCount));
+                    writer.write(String.format("%s, %d, %s, %s, %d, %s, %d",
+                            word, count, percentWithPoint, mostCommonWord, mostCommonWordCount, rarestWord, rarestWordCount));
                 } else {
                     writer.write(String.format("%s, %d, %s", word, count, percentWithPoint));
                 }
                 writer.newLine();
             }
-            writer.close();
-
         } catch (IOException ex) {
-            System.err.println("Ошибка при записи в CSV-файл: " + ex);
+            System.err.println("Ошибка при записи в CSV-файл: " + ex + "\n");
         }
-        System.out.println("CSV-файл успешно создан: word_frequency.csv");
+        System.out.println("CSV-файл успешно создан: " + inputFileName + "_frequency_analysis.csv\n");
+
     }
 }
