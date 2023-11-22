50111404738cf028558e77b4fe5e341771ad2a5c
Nikita Sergeev <sergeevn660@gmail.com>
2023-11-22 17:15:30 +0300

    Создан класс с тестированием AutoWateringTest

--- Java_LR5_Maven/src/test/java/AutoWateringTest.java
+++ Java_LR5_Maven/src/test/java/AutoWateringTest.java
@@ -4,7 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AutoWateringTest {
     @org.junit.jupiter.api.Test
     public void turnOnOffTest() {
-        AutoWatering autoWatering = new AutoWatering("Karcher", "Model_1", "бензин", 2010, 8);
+        AutoWatering autoWatering = new AutoWatering("Rain Bird", "MODEL_81", "солнечные панели", 2014, 10);
+
         autoWatering.switchOn();
         assertTrue(autoWatering.getOnOrOff());
 
@@ -12,5 +13,41 @@ public class AutoWateringTest {
         assertFalse(autoWatering.getOnOrOff());
     }
 
+    public void adjustWorkingMinutesTest() {
+        AutoWatering autoWatering = new AutoWatering("Rain Bird", "MODEL_81", "солнечные панели", 2014, 10);
 
+        autoWatering.adjustWorkingMinutes(5);
+        assertEquals(5, autoWatering.getWorkingMinutes());
+
+        autoWatering.adjustWorkingMinutes(30);
+        assertEquals(30, autoWatering.getWorkingMinutes());
+
+        autoWatering.adjustWorkingMinutes(90);
+        assertEquals(90, autoWatering.getWorkingMinutes());
+
+        autoWatering.adjustWorkingMinutes(0);
+        assertEquals(10, autoWatering.getWorkingMinutes());
+
+        autoWatering.adjustWorkingMinutes(100);
+        assertEquals(10, autoWatering.getWorkingMinutes());
+    }
+
+    public void adjustWaterPressureTest() {
+        AutoWatering autoWatering = new AutoWatering("Rain Bird", "MODEL_81", "солнечные панели", 2014, 10);
+
+        autoWatering.adjustWaterPressure(20);
+        assertEquals(20, autoWatering.getWaterPressure());
+
+        autoWatering.adjustWaterPressure(60);
+        assertEquals(60, autoWatering.getWaterPressure());
+
+        autoWatering.adjustWaterPressure(80);
+        assertEquals(80, autoWatering.getWaterPressure());
+
+        autoWatering.adjustWaterPressure(0);
+        assertEquals(40, autoWatering.getWaterPressure());
+
+        autoWatering.adjustWaterPressure(100);
+        assertEquals(40, autoWatering.getWaterPressure());
+    }
 }
