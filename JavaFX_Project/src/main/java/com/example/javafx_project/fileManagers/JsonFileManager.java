ad024a5217d2ac50778723d11b56d10680e686dc
Nikita Sergeev <sergeevn660@gmail.com>
2024-03-04 13:29:37 +0300

    Добавлен класс для работы с JSON

---
+++ JavaFX_Project/src/main/java/com/example/javafx_project/fileManagers/JsonFileManager.java
@@ -0,0 +1,86 @@
+package com.example.javafx_project.fileManagers;
+
+import com.example.javafx_project.devices.AutoWatering;
+import com.example.javafx_project.devices.GardeningDevice;
+import com.example.javafx_project.devices.Lawnmower;
+import com.example.javafx_project.devices.ThermalDrive;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonParseException;
+
+import java.io.*;
+import java.time.LocalDate;
+
+public class JsonFileManager {
+    private static final Logger logger = LogManager.getLogger("FilesLogger");
+
+    private static boolean isValidFileNameJSON(String filename) {
+        try {
+            JsonParser parser = new JsonParser();
+            parser.parse(filename + ".json");
+            return true;
+        } catch (JsonParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean writeToJSON(GardeningDevice device, String filename) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter(filename)) {
+            gson.toJson(device, writer);
+            logger.info("Успешная запись устройства с ID = " + device.getId() + "в JSON-файл: " + filename);
+            return true;
+        } catch (IOException e) {
+            logger.error("Ошибка при записи в JSON-файл");
+        }
+        return false;
+    }
+
+    public static GardeningDevice readFromJSON(File file) {
+        if (file.exists()) {
+            StringBuilder contentJSON = new StringBuilder();
+            try {
+                String line;
+                FileReader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                while ((line = bufferedReader.readLine()) != null) {
+                    contentJSON.append(line);
+                }
+                String jsonString = contentJSON.toString();
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                GardeningDevice jsonObject = null;
+
+                try {
+                    jsonObject = gson.fromJson(jsonString, Lawnmower.class);
+                    switch (jsonObject.getType()) {
+                        case "Lawnmower" -> jsonObject = gson.fromJson(jsonString, Lawnmower.class);
+                        case "AutoWatering" -> jsonObject = gson.fromJson(jsonString, AutoWatering.class);
+                        case "ThermalDrive" -> jsonObject = gson.fromJson(jsonString, ThermalDrive.class);
+                        default ->
+                            logger.error("Ошибка при чтении из JSON: содержимое JSON-файла не соответствует ни одному устройству");
+                    }
+                } catch (RuntimeException e) {
+                    logger.error("Ошибка при чтении из JSON");
+                }
+
+                if (jsonObject != null) {
+                    if (jsonObject.getType().equals("Lawnmower") || jsonObject.getType().equals("AutoWatering") || jsonObject.getType().equals("ThermalDrive")) {
+                        logger.info("Информация об устройстве типа " + jsonObject.getType() + " успешно получена из JSON");
+                    } else {
+                        logger.error("Ошибка при чтении из JSON: данные не идентифицированы");
+                    }
+                    return jsonObject;
+                }
+
+            } catch (IOException e) {
+                logger.error("Ошибка при чтении JSON-файла");
+            }
+        } else {
+            logger.error("Ошибка при чтении из JSON: файл с таким именем не найден");
+        }
+        return null;
+    }
+}
