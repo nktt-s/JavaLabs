c53bcdb077e8e83eda9e16e06fe96022865f2582
Nikita Sergeev <sergeevn660@gmail.com>
2024-04-23 22:38:58 +0300

    Обновлена зависимость в pom.xml

--- JavaFX_Project/src/main/java/module-info.java
+++ JavaFX_Project/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.javafx_project {
     requires javafx.fxml;
     requires java.sql;
     requires org.apache.logging.log4j;
-    requires mysql.connector.java;
+    requires mysql.connector.j;
     requires com.google.gson;
 
 
